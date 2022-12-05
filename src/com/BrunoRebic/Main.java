1ec81d7685a714f53c36d0479ac75784a37dac63
Bruno Rebic <119545220+brunorebic@users.noreply.github.com>
2022-12-05 12:38:13 +0100

    Update Main.java

--- src/com/BrunoRebic/Main.java
+++ src/com/BrunoRebic/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final Locations locations = new Locations();
 
     public static void main(String[] args) throws IOException {
-//        Locations.printLocationsAndExits();//optional - print all locations and exists
+        Locations.printLocationsAndExits();//optional - print all locations and exists
 
         Scanner scanner = new Scanner(System.in);
 
